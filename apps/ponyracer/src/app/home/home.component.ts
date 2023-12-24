52a01d601328cdb68c177b930f011b0ec160b8b0
Gaëtan Bloch <gaetan.bloch@keyconsulting.fr>
2023-12-24 17:46:17 +0100

    feat(routing): add some routing

--- apps/ponyracer/src/app/home/home.component.ts
+++ apps/ponyracer/src/app/home/home.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { SvgIconComponent } from '@geode/components';
+import { RouterLink } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'angular-monorepo-home',
   standalone: true,
-  imports: [CommonModule, HlmButtonDirective, SvgIconComponent],
+  imports: [CommonModule, HlmButtonDirective, SvgIconComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  title = inject(Title);
+
+  constructor() {
+    this.title.setTitle('Ponyracer - Home');
+  }
+}
